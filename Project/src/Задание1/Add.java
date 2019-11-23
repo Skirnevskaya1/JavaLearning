4dd6f1bc4f267ed77c6637282ebe91460fa943c6
Skirnevskaya1 <57950489+Skirnevskaya1@users.noreply.github.com>
2019-11-23 19:29:02 +0700

    задание

---
+++ Project/src/Задание1/Add.java
@@ -0,0 +1,15 @@
+package Задание1;
+
+import java.util.Scanner;
+
+/**
+ * Created by User on 23.11.2019.
+ */
+public class Add {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + b);
+    }
+}
---
+++ Project/src/Задание1/MyClass.java
@@ -0,0 +1,10 @@
+package Задание1;
+
+/**
+ * Created by User on 23.11.2019.
+ */
+public class MyClass {
+    public static void main(String args[]) {
+        System.out.println("Java мой друг, и мы друг друга понимаем");
+    }
+}
