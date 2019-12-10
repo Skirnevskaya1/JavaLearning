0ee1cf90a4d3f6276db4b0528c7e4f338bd5d6eb
Skirnevskaya1 <57950489+Skirnevskaya1@users.noreply.github.com>
2019-12-10 20:25:45 +0700

    task

--- lesson3/Task6.java
+++ lesson3/Task6.java
@@ -7,9 +7,8 @@ public class Task6 {
     //Дано натуральное число N. Выведите слово YES, если число N является точной степенью двойки, или слово NO в противном случае.
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        if (a % 2 == 1)
-            System.out.println("NO");
-        else System.out.println("YES");
+        Integer a = scanner.nextInt();
+        boolean b = ((a & (a - 1)) == 0);
+        System.out.println(b);
     }
 }
