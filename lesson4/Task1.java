1d4526e2bf7718c382ea6c1deaef443712caf6eb
Skirnevskaya1 <57950489+Skirnevskaya1@users.noreply.github.com>
2019-12-16 22:02:31 +0700

    task

--- lesson4/Task1.java
+++ lesson4/Task1.java
@@ -1,18 +1,33 @@
 import java.util.Scanner;
 
 /**
- * Created by User on 12.12.2019.
+ * Created by User on 15.12.2019.
  */
 public class Task1 {
+
     //Запишите условия для точек, принадлежащих выделенной области
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите x: ");
-        int x = Integer.parseInt(scanner.next());
+        int x = scanner.nextInt();
         System.out.println("Введите y: ");
-        int y = Integer.parseInt(scanner.next());
-        if ((x == 4 && y == 0) || (x == -4 && y == 0) || (x == 0 && y == 6))
+        int y = scanner.nextInt();
+        int x1 = 4, y1 = 0, x2 = 0, y2 = -6;
+        int x3 = 4, y3 = 0, x4 = -4, y4 = 0;
+        int x5 = -4, y5 = 0, x6 = 0, y6 = -6;
+
+        int a1 = y1 - y2;
+        int b1 = x2 - x1;
+        int c1 = x1 * y2 - x2 * y1;
+        int a2 = y3 - y4;
+        int b2 = x4 - x3;
+        int c2 = x3 * y4 - x4 * y3;
+        int a3 = y5 - y6;
+        int b3 = x6 - x5;
+        int c3 = x5 * y6 - x6 * y5;
+
+        if (a1 * x + b1 * y + c1 <= 0 && a2 * x + b2 * y + c2 >= 0 && a3 * x + b3 * y + c3 >= 0) {
             System.out.println("Принадлежит выделенной области");
-        else System.out.println("Не принадлежит выделенной области");
+        } else System.out.println("Не принадлежит выделенной области");
     }
 }
--- lesson4/Task2.java
+++ lesson4/Task2.java
@@ -8,15 +8,26 @@ public class Task2 {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите x: ");
-        int x = Integer.parseInt(scanner.next());
+        int x = scanner.nextInt();
         System.out.println("Введите y: ");
-        int y = Integer.parseInt(scanner.next());
-        if ((x == -4 && y == 4) || (x == -1 && y == 4))
-            System.out.println("Принадлежит выделенной области круга");
-        else if ((x == 0 && y == 0) || (x == 6 && y == 0) || (x == 0 && y == 6))
-            System.out.println("Принадлежит выделенной области");
-        else
-            System.out.println("Не принадлежит выделенной области");
+        int y = scanner.nextInt();
+        int x1 = 0, y1 = 0, x2 = 6, y2 = 0;
+        int x3 = 0, y3 = 0, x4 = 0, y4 = 6;
+        int x5 = 0, y5 = 6, x6 = 6, y6 = 0;
+        int x7 = -4, y8 = 4;
 
+        int a1 = y1 - y2;
+        int b1 = x2 - x1;
+        int c1 = x1 * y2 - x2 * y1;
+        int a2 = y3 - y4;
+        int b2 = x4 - x3;
+        int c2 = x3 * y4 - x4 * y3;
+        int a3 = y5 - y6;
+        int b3 = x6 - x5;
+        int c3 = x5 * y6 - x6 * y5;
+
+        if (a1 * x + b1 * y + c1 >= 0 && a2 * x + b2 * y + c2 <= 0 && a3 * x + b3 * y + c3 <= 0 || (x - x7) * (x - x7) + (y - y8) * (y - y8) <= 9) {
+            System.out.println("Принадлежит выделенной области");
+        } else System.out.println("Не принадлежит выделенной области");
     }
 }
