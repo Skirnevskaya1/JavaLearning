b7fa7c31d6aecde9e57f03d43faf207d6fe2d9fc
Skirnevskaya1 <57950489+Skirnevskaya1@users.noreply.github.com>
2020-01-06 23:44:04 +0700

    task

---
+++ lesson6/ArrayGenerator.java
@@ -0,0 +1,22 @@
+/**
+ * Created by User on 06.01.2020.
+ */
+public class ArrayGenerator {
+    public static int[] getIntArray(int length, int max, int min) {
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) {
+            arr[i] = (int) (Math.random() * (max - min + 1) + min);
+           System.out.print(arr[i] + " ");
+        }
+        return arr;
+    }
+        public static double[] getDoubleArray(int length, double max, double min){
+
+            double[] array = new double[length];
+            for (int i = 0; i < length; i++) {
+                array[i] = (double) (Math.random() * (max - min + 1) + min);
+                System.out.print(array[i]+" ");
+            }
+            return array;
+        }
+    }
---
+++ lesson6/Task3_1.java
@@ -0,0 +1,15 @@
+/**
+ * Created by User on 06.01.2020.
+ */
+public class Task3_1 {
+    public static void main(String[] args) {
+        int[] myIntArray = ArrayGenerator1.getIntArray(2,2);
+
+    }
+
+    public static void sumArray(){
+        int max=0;
+        if (q > max) {
+            max = q;
+    }
+}
