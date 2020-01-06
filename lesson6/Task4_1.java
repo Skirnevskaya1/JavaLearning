b6fa7dbafafb9cda2c343cd909c30937e352f22b
Skirnevskaya1 <57950489+Skirnevskaya1@users.noreply.github.com>
2020-01-06 23:44:59 +0700

    task

---
+++ lesson6/Task4_1.java
@@ -0,0 +1,11 @@
+/**
+ * Created by User on 06.01.2020.
+ */
+public class Task4_1 {
+    public static void main(String[] args) {
+        int[] myIntArray = ArrayGenerator.getIntArray(10, 0, 15);
+        System.out.print("\n");
+        double[] myDoubleArray = ArrayGenerator.getDoubleArray(10, 0, 15);
+
+    }
+}
