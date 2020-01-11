8d40a58eb810d1cfbe602a9b97570407af49b002
Skirnevskaya1 <57950489+Skirnevskaya1@users.noreply.github.com>
2020-01-11 20:29:19 +0700

    task

--- lesson6/Task3_1.java
+++ lesson6/Task3_1.java
@@ -6,34 +6,29 @@ import java.util.Scanner;
 public class Task3_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число строк - ");
-        int lengthA = scanner.nextInt();
-        System.out.println("Введите число столбцов - ");
+        System.out.println("Введите количество массивов - ");
+        int lengthcount = scanner.nextInt();
+        System.out.println("Введите длину массива - ");
         int lengthB = scanner.nextInt();
-        int[][] array = new int[lengthA][lengthB];
 
-        ArrayGenerator1.getIntArray(array, lengthA, lengthB);
-        getSumArray(array, lengthA, lengthB);
+        int[][] array = new int[lengthcount][lengthB];
+        int max = 500;
+        int min = 1;
+
+        for (int i=0;i < array.length; i++) {
+           array[i] = ArrayGenerator.getIntArray(lengthcount, max, min);
+        }
+        for (int i=0;i < array.length; i++) {
+            getSumArray(array[i]);
+        }
     }
 
-    public static int[][] getSumArray(int[][] array, int lengthA, int lengthB) {
-
-        int a = 0;
-        int b = 0;
-        int sum = 0;
-
-        for (int i = 0; i < lengthA; i++) {
-            for (int j = 0; j < lengthB; j++) {
-                sum += array[i][j];
-            }
-            if (a < sum) {
-                a = sum;
-                b = i;
-            }
+    public static int getSumArray(int[] array) {
+        int sum =0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
         }
-        System.out.println(b + "-й массив с максимальной суммой элементов: " + "\nСумма строки ровна = " + sum);
-
-
-        return array;
+        System.out.println("Сумма равна = "+sum);
+        return sum;
     }
 }
