ccf8cbdaa00e9ac64984cffb890ecdd93c4f0dfc
Skirnevskaya1 <57950489+Skirnevskaya1@users.noreply.github.com>
2020-01-11 23:37:05 +0700

    task

--- lesson6/Task1.java
+++ lesson6/Task1.java
@@ -13,38 +13,24 @@ public class Task1 {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         String num = scanner.nextLine();
-
         while (true) {
             break;
         }
         String numbers1 = String.valueOf(num.split(" "));
-        // for (String value : numbers) {
-        //     list.add(value);
-
-        int numbers =Integer.parseInt(numbers1);
-
+        int numbers = Integer.parseInt(numbers1);
         System.out.print("среднее арифметическое чисел равно: " + average(numbers));
     }
 
-    public static double average(int numbers) {
-        float s = 0;
-        int n = 1;
-        for (; numbers != -1;) {
-            s = (s + numbers);
-            //numbers = Integer.parseInt(numbers1);
-            n++;
+    public static double average(int[] numbers) {
+
+        double average1 = 0;
+        if (numbers.length > 0) {
+            double sum = 0;
+            for (int j = 0; j < numbers.length; j++) {
+                sum += numbers[j];
+            }
+            average1 = sum / numbers.length;
         }
-
-
-//        double average1 = 0;
-//        if (numbers.length > 0) {
-//            double sum = 0;
-//            for (int j = 0; j < numbers.length; j++) {
-//                sum += numbers[j];
-//            }
-//            average1 = sum / numbers.length;
-//        }
-//        return average1;
-        return (s / (n - 1));
+        return average1;
     }
 }
