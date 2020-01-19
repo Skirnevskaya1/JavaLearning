2486ce0fdb1c31f40eda273f238e29975e89364d
Skirnevskaya1 <57950489+Skirnevskaya1@users.noreply.github.com>
2020-01-19 20:42:16 +0700

    task

--- lesson7/Task2.java
+++ lesson7/Task2.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число: ");
-        int x = new Integer(Math.abs(scanner.nextInt()));
+        int x = new Integer(scanner.nextInt());
 
         // Бинарный формат числа
         String convert = Integer.toBinaryString(x);
@@ -20,10 +21,14 @@ public class Task2 {
         // помещается ли число х в ячейке типа byte
         if (x == (byte) x) {
             System.out.println("YES");
-        } else System.out.println("NO");
+        } else {
+            System.out.println("NO");
+        }
 
         if (x == (short) x) {
             System.out.println("YES");
-        } else System.out.println("NO");
+        } else {
+            System.out.println("NO");
+        }
     }
 }
