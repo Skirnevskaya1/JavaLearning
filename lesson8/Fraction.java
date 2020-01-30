c0590e3872a3aaac60caa279a85d007b5a0d90ff
Skirnevskaya1 <57950489+Skirnevskaya1@users.noreply.github.com>
2020-01-30 23:58:49 +0700

    task

--- lesson8/Fraction.java
+++ lesson8/Fraction.java
@@ -1,28 +1,25 @@
-/**
- * Created by User on 18.01.2020.
- */
 public class Fraction {
 
     public static void main(String[] args) {
-        Fraction f = new Fraction(5, 4);
-        Fraction f2 = new Fraction(3, 6);
+        Fraction f = new Fraction(18, 4);
+        Fraction f2 = new Fraction(2, 4);
         f.multiplication(f2);
         System.out.print("Multiplication : ");
         f.print();
         System.out.print("Reduction : ");
-        f.reduction(f2);
+        f.reduction();
         f.print();
-        f.selectIntPart(f2);
+        f.selectIntPart();
         System.out.print("Select Int Part : ");
         f.print();
         System.out.println("------------------------------------");
-        f.division(f2);
+        f2.division(f);
         System.out.print("Division : ");
-        f.print();
+        f2.print();
         System.out.print("Reduction : ");
-        f.reduction(f2);
+        f.reduction();
         f.print();
-        f.selectIntPart(f2);
+        f.selectIntPart();
         System.out.print("Select Int Part : ");
         f.print();
         System.out.println("------------------------------------");
@@ -30,9 +27,9 @@ public class Fraction {
         System.out.print("Subtraction : ");
         f.print();
         System.out.print("Reduction : ");
-        f.reduction(f2);
+        f.reduction();
         f.print();
-        f.selectIntPart(f2);
+        f.selectIntPart();
         System.out.print("Select Int Part : ");
         f.print();
         System.out.println("------------------------------------");
@@ -40,9 +37,9 @@ public class Fraction {
         System.out.print("Addition : ");
         f.print();
         System.out.print("Reduction : ");
-        f.reduction(f2);
+        f.reduction();
         f.print();
-        f.selectIntPart(f2);
+        f.selectIntPart();
         System.out.print("Select Int Part : ");
         f.print();
     }
@@ -56,13 +53,13 @@ public class Fraction {
     }
 
     public void multiplication(Fraction f2) {
-        numerator = numerator * f2.numerator;
-        denominator = denominator * f2.denominator;
+        numerator *= f2.numerator;
+        denominator *= f2.denominator;
     }
 
     public void division(Fraction f2) {
-        numerator = f2.denominator * numerator;
-        denominator = denominator * f2.numerator;
+        numerator *= f2.denominator;
+        denominator *= f2.numerator;
     }
 
     public void subtraction(Fraction f2) {
@@ -72,33 +69,33 @@ public class Fraction {
 
     public void addition(Fraction f2) {
         numerator = (numerator * f2.denominator) + (f2.numerator * denominator);
-        denominator = f2.denominator * denominator;
+        denominator *= f2.denominator;
     }
 
     private static double gcd(double numerator, double denominator) {
         while (denominator != 0) {
-            double tmp = (numerator) % denominator;
+            double tmp = numerator % denominator;
             numerator = denominator;
             denominator = tmp;
         }
         return numerator;
     }
 
-    public void reduction(Fraction f2) {
+    public void reduction() {
         double n = gcd(numerator, denominator);
-        numerator = numerator / n;
-        denominator = denominator / n;
+        numerator /= n;
+        denominator /= n;
     }
 
-    public void selectIntPart(Fraction f2) {
+    public void selectIntPart() {
         if (numerator >= denominator) {
-            numerator %= denominator;
+            numerator = numerator/denominator;
+            denominator = 0;
         }
     }
 
     public double conversion(Fraction fraction) {
-        double res = (double) (fraction.numerator / fraction.denominator);
-        return res;
+        return (double) (fraction.numerator / fraction.denominator);
     }
 
     public void print() {
