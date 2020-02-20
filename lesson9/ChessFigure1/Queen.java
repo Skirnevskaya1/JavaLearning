7ad9d302614d25e6068b33652a372462279cce4b
Skirnevskaya1 <57950489+Skirnevskaya1@users.noreply.github.com>
2020-02-20 21:54:54 +0700

    task

--- lesson9/ChessFigure1/Queen.java
+++ lesson9/ChessFigure1/Queen.java
@@ -9,7 +9,7 @@ public class Queen extends ChessFigure {
     @Override
     public void move(int x, int y) {
         System.out.println("Хожу на эти координаты--> " + x + ":" + y + ",  местонахождение --> " + this.x + ":" + this.y);
-        if ((isPossible(x, y)) && (!((x != this.x) && (y != this.y))) || (isPossible(x, y)) && (Math.abs(x - this.x) == Math.abs(y - this.y))) {
+        if ((isPossible(x, y)) && ((!((x != this.x) && (y != this.y))) || (Math.abs(x - this.x) == Math.abs(y - this.y)))) {
             System.out.println("Ход возможен у ферзя");
         } else {
             System.out.println("Ход невозможен у ферзя");
