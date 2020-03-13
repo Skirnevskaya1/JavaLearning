547db7e5402d2ede0afecc567130bd528bc79747
Kate <57950489+Skirnevskaya1@users.noreply.github.com>
2020-03-13 17:38:33 +0700

    Update Inspector.java

--- lesson10/Inspector.java
+++ lesson10/Inspector.java
@@ -7,7 +7,7 @@ public class Inspector extends Person {
     public void checkBuildings(Building building) {
         for (Apartment floor : building.floors) {
             for (Person person : floor.residences) {
-                if (getRating() <= 1000) {
+                if (person.getRating() <= 1000) {
                     floor.residences.remove(getRating());
                 }
                 System.out.println(person.getRating());
