dec681038c82783d3b510cd9ca9a9401ef317f85
Skirnevskaya1 <57950489+Skirnevskaya1@users.noreply.github.com>
2020-03-13 23:03:54 +0700

    task

---
+++ lesson10/Building.java
@@ -0,0 +1,35 @@
+import java.util.LinkedList;
+
+public class Building {
+    LinkedList<Apartment> floors = new LinkedList<>();
+
+    public Building(int amountFloor) {
+        for (int i = 0; i < amountFloor; i++) {
+            floors.add(new Apartment(3));
+        }
+    }
+
+    public boolean add(Person person) {
+        for (Apartment floor : floors) {
+            if (floor.addPerson(person)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Person remove(int floor, int index) {
+        return floors.get(floor).remove(index);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (Apartment floor : floors) {
+            builder.append(floor.toString()).append("\n");
+        }
+        return builder.toString();
+    }
+
+
+}
