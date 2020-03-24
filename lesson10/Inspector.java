97506a31b72f1115aeefcf3ab46138869f95fde9
Kate <57950489+Skirnevskaya1@users.noreply.github.com>
2020-03-24 12:35:48 +0700

    Update Inspector.java

--- lesson10/Inspector.java
+++ lesson10/Inspector.java
@@ -7,9 +7,13 @@ public class Inspector extends Person {
     public void checkBuildings(Building building) {
         for (Apartment floor : building.floors) {
             for (int i = 0; i < floor.residences.size(); i++) {
-                if (floor.residences.get(i).getRating() <= 1000) {
-                    floor.remove(getRating());
-                    System.out.println(floor.residences.get(i).getName() + " выселен из квартиры из-за низкого рейтинга = " + floor.residences.get(i).getRating());
+                if (floor.residences.get(i) == null) {
+
+                } else {
+                    if (floor.residences.get(i).getRating() <= 1000) {
+                        floor.remove(getRating());
+                        System.out.println(floor.residences.get(i).getName() + " выселен из квартиры из-за низкого рейтинга = " + floor.residences.get(i).getRating());
+                    }
                 }
             }
         }
