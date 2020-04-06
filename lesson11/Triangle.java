597d63546fd17374eb697c4c0c0a3b687385e3ba
Kate <57950489+Skirnevskaya1@users.noreply.github.com>
2020-04-06 15:48:41 +0700

    Update Triangle.java

--- lesson11/Triangle.java
+++ lesson11/Triangle.java
@@ -1,22 +1,10 @@
 public class Triangle extends Figure implements Rotatable, Moveable {
-    private double x1;
-    private double y1;
-    private double x2;
-    private double y2;
-    private double x3;
-    private double y3;
     private Point point1;
     private Point point2;
     private Point point3;
 
     public Triangle(String name, double x1, double y1, double x2, double y2, double x3, double y3) {
         super(name);
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
-        this.x3 = x3;
-        this.y3 = y3;
         this.point1 = new Point(x1, y1);
         this.point2 = new Point(x2, y2);
         this.point3 = new Point(x3, y3);
