b7549fee02acaad8b272d74b2f828a590f4625f7
Kate <57950489+Skirnevskaya1@users.noreply.github.com>
2020-04-06 15:26:17 +0700

    Update Point.java

--- lesson11/Point.java
+++ lesson11/Point.java
@@ -1,6 +1,22 @@
 public class Point implements Moveable {
-    protected double x;
-    protected double y;
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    private double x;
+    private double y;
 
     public Point(double x, double y) {
         this.x = x;
