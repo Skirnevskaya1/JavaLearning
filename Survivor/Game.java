f62f677e62bdc864d812c7e19ab4a349ad68d994
Skirnevskaya1 <57950489+Skirnevskaya1@users.noreply.github.com>
2020-05-02 00:07:53 +0700

    task

---
+++ Survivor/Game.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Game {
+    public static Character character;
+    public static Story story;
+
+    public static void main(String[] args) throws InterruptedException {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Имя выжившего: ");
+        character = new Character(in.next());
+        story = new Story(character.name);
+
+        while (true) {
+            character.health += story.current_situation.dHealth;
+            character.experience += story.current_situation.dExperience;
+            character.days += story.current_situation.dDays;
+<<<<<<< HEAD
+            System.out.println("\nЗдоровье:" + character.health + "\tОпыт:" + character.experience + "\tДни:" + character.days + "\n");
+            System.out.println("=============" + story.current_situation.subject + "==============");
+=======
+            //  Thread.sleep(2000);
+            System.out.println("\nЗдоровье:" + character.health + "\tОпыт:" + character.experience + "\tДни:" + character.days + "\n");
+            System.out.println("=============" + story.current_situation.subject + "==============");
+            //  Thread.sleep(2000);
+>>>>>>> d31e1fb6e1a824a01024f52ca6bd15bdbe9e51ab
+            System.out.println(story.current_situation.text);
+            if (story.isEnd()) {
+                return;
+            }
+            story.go(in.nextInt());
+        }
+    }
+}
