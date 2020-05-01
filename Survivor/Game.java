97469dec9e2baa98c5a4ed6ec18feaf1b0e6e008
Skirnevskaya1 <57950489+Skirnevskaya1@users.noreply.github.com>
2020-05-02 00:01:53 +0700

    task

--- Survivor/Game.java
+++ Survivor/Game.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Game  {
+public class Game {
     public static Character character;
     public static Story story;
 
@@ -8,16 +8,14 @@ public class Game  {
         Scanner in = new Scanner(System.in);
         System.out.println("Имя выжившего: ");
         character = new Character(in.next());
-        story = new Story();
+        story = new Story(character.name);
 
         while (true) {
             character.health += story.current_situation.dHealth;
             character.experience += story.current_situation.dExperience;
             character.days += story.current_situation.dDays;
-          //  Thread.sleep(2000);
             System.out.println("\nЗдоровье:" + character.health + "\tОпыт:" + character.experience + "\tДни:" + character.days + "\n");
             System.out.println("=============" + story.current_situation.subject + "==============");
-          //  Thread.sleep(2000);
             System.out.println(story.current_situation.text);
             if (story.isEnd()) {
                 return;
