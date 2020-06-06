27984708f95ad6360ab6e395695b54490932f149
Skirnevskaya1 <57950489+Skirnevskaya1@users.noreply.github.com>
2020-06-06 21:17:35 +0700

    task

--- lesson12/Task7.java
+++ lesson12/Task7.java
@@ -4,6 +4,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Task7 {
@@ -11,9 +12,12 @@ public class Task7 {
         Document document = Jsoup.connect("https://www.dns-shop.ru/catalog/17a8a01d16404e77/smartfony/?order=1&groupBy=none&brand=apple&f[pqc]=kdf7y-o8r3o&stock=2").get();
         Elements elementsLinks = document.select("a[class=ui-link]");
         Iterator<Element> iterator = elementsLinks.iterator();
-        
+        ArrayList<Elements> arrayList = new ArrayList<>();
+
         while (iterator.hasNext()) {
             Element element = iterator.next();
+            arrayList.add(elementsLinks);
+
             String name = element.select("h1[class=page-title price-item-title]").text();
             String price = element.select("span[class=current-price-value]").text();
             System.out.println(name + " " + price);
