ba846d081d2036c5eb28f5f896a1b04ce869641d
Skirnevskaya1 <57950489+Skirnevskaya1@users.noreply.github.com>
2020-06-08 20:45:47 +0700

    task

--- lesson12/Task7.java
+++ lesson12/Task7.java
@@ -1,3 +1,4 @@
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -11,18 +12,21 @@ public class Task7 {
         Document document = Jsoup.connect("https://www.dns-shop.ru/catalog/17a8a01d16404e77/smartfony/?order=1&groupBy=none&brand=apple&f[pqc]=kdf7y-o8r3o&stock=2").get();
         Elements elementsLinks = document.select("a[class=ui-link]");
         ArrayList<String> urls = new ArrayList<>();
-
+        ArrayList<String> urls2 = new ArrayList<>();
         for (Element elements : elementsLinks) {
             urls.add(elements.attr("href"));
         }
         for (int i = 0; i < urls.size(); i++) {
             if (urls.get(i).startsWith("/product/")) {
-             //     System.out.println(urls.get(i));
-                Document document1 = Jsoup.parse(urls.get(i));
-                String name = document1.select("h1[class=page-title price-item-title]").text();
-                String price = document1.select("span[class=current-price-value]").text();
-                System.out.println(name + " " + price);
+                urls2.add("https://www.dns-shop.ru" + urls.get(i));
             }
+        }
+        for (int i = 0; i < urls2.size(); i++) {
+            //  System.out.println(urls2.get(i));
+            Document document1 = (Document) Jsoup.connect(urls.get(i));
+            String name = document1.select("h1[class=page-title price-item-title]").text();
+            String price = document1.select("span[class=current-price-value]").text();
+            System.out.println(name + " " + price);
         }
     }
 }
