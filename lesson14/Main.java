f0ccd4f53082049668dcc1edeb5b54ce241d9b8a
Kate <57950489+Skirnevskaya1@users.noreply.github.com>
2020-07-24 10:00:00 +0700

    Update Main.java

--- lesson14/Main.java
+++ lesson14/Main.java
@@ -1,18 +1,14 @@
 public class Main {
     static Service service = new Service();
-    static Artist artist;
-    static Listener listener;
 
     public static void main(String[] args) throws InterruptedException {
 
         for (int i = 1; i < 10; i++) {
-            Thread threadArtist = new Thread(artist.getInstance());
-            threadArtist.run();
-            //threadArtist.start();
+            Thread threadArtist = new Thread(Artist.getInstance());
+            threadArtist.start();
 
-            Thread threadListener = new Thread(listener.getInstance());
-            threadListener.run();
-            // threadListener.start();
+            Thread threadListener = new Thread(Listener.getInstance());
+            threadListener.start();
             int time = (int) (Math.random() * (5000 - 1000) + 1000);
             Thread.sleep(time);
         }
